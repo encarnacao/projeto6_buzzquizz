bad779311b1488df56edddf6f13e172670a0ada0
Ludmilahttps <ludmila.https@gmail.com>
2022-11-06 01:37:48 -0300

    carrega os quizzes do servidor

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -1,7 +1,30 @@
+let feed = document.querySelector('.view')
+let id;
+
+
 init();
 
 function init()
 {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promise.then(pullquizzes);
+    promise.catch((error) => {
+        alert(error);
+    });
+    
     /*se não tiver quizz add none em seus quizzes*/
     /* senão add none em add none em criar quizz*/
+}
+
+function pullquizzes(promise)
+{
+    for (let i = 0; i < promise.data.length; i++) {
+        console.log(promise.data[i]); //printa quizz por quizz
+        feed.innerHTML += `<button class="box quizz" id="${promise.data[i].id}" onclick="window.location.href = 'quizz.html'">
+                                <p class="NameQuiz">${promise.data[i].title}</p>
+                            </button>`;
+        
+    }
+    id = document.getElementById(16899);
+    id.style.background = "";
 }
