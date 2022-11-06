321e83a34348a70bfb28bcce48bbf4d00f54c50e
encarnacao <cencarnacaoq@gmail.com>
2022-11-06 10:50:53 -0300

    Alterado como os quizzes são dispostos no Index. Classes e responsividade

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -1,30 +1,40 @@
 let feed = document.querySelector('.view')
+let quizzes;
 let id;
 
 
 init();
 
-function init()
-{
+function init() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promise.then(pullquizzes);
     promise.catch((error) => {
         alert(error);
     });
-    
+
     /*se não tiver quizz add none em seus quizzes*/
     /* senão add none em add none em criar quizz*/
 }
 
-function pullquizzes(promise)
-{
-    for (let i = 0; i < promise.data.length; i++) {
-        console.log(promise.data[i]); //printa quizz por quizz
-        feed.innerHTML += `<button class="box quizz" id="${promise.data[i].id}" onclick="window.location.href = 'quizz.html'">
-                                <p class="NameQuiz">${promise.data[i].title}</p>
+function pullquizzes(promise) {
+    quizzes = promise.data;
+    renderizeQuizzes();
+}
+
+function renderizeQuizzes(){
+    feed.innerHTML = "";
+    for (let i = 0; i < quizzes.length; i++) {
+        console.log(quizzes[i]); //printa quizz por quizz
+        feed.innerHTML += `<div class="quizz-image" id="${quizzes[i].id}" onclick="goToQuizz(this)">
+                                <p class="titulo-quizz">${quizzes[i].title}</p>
                             </button>`;
-        
+        feed.children[i].style.backgroundImage = `url(${quizzes[i].image})`;
     }
-    id = document.getElementById(16899);
-    id.style.background = "";
+
+}
+
+function goToQuizz(quizz){
+    id = quizz.getAttribute("id");
+    console.log(id);
+    window.location.href = 'quizz.html';
 }
