fe604a71fab4145c2e51e12d51e08050dbcb8fa5
Ludmilahttps <ludmila.https@gmail.com>
2022-11-07 02:51:28 -0300

    tentativa de implementar o bonus 2

--- assets/js/scriptIndex.js
+++ assets/js/scriptIndex.js
@@ -1,6 +1,8 @@
 const feed = document.querySelector('.view')
 const loading = document.querySelector('.loading');
 const container = document.querySelector('.container');
+//let userCreatedQuizzId = JSON.parse(localStorage.getItem("quizzes"))
+let localQuizzes = localStorage.getItem("quizzes");
 let quizzes;
 let id;
 
@@ -13,17 +15,6 @@ function init() {
         alert(error);
         window.location.reload();
     });
-    quizzes = promise.data;
-    /*se não tiver quizz add hidden em seus quizzes*/
-    // if ( == null) {
-    //     withQuizz.classList.add("hidden");
-    //     alert();
-    // } /* senão add none em add hidden em criar quizz*/
-    // else {
-    //     alert();
-    //     NoQuizz.classList.add("hidden");
-    //     render();
-    // }
 }
 
 function pullquizzes(promise) {
@@ -33,23 +24,65 @@ function pullquizzes(promise) {
     container.classList.remove("hidden");
 }
 
-function renderizeQuizzes(){
+function renderizeQuizzes() {
     feed.innerHTML = "";
     for (let i = 0; i < quizzes.length; i++) {
         //console.log(quizzes[i]); //printa quizz por quizz
         feed.innerHTML += `<div class="quizz-image box" id="${quizzes[i].id}" onclick="goToQuizz(this)">
                                 <p class="titulo-quizz">${quizzes[i].title}</p>
+                                <div class="edit-delete">
+                                    <ion-icon name="create-outline" onclick="deleteQuizz()"></ion-icon>
+                                    <ion-icon name="trash" onclick="deleteQuizz()"></ion-icon>
+                                </div>
                             </div>`;
         feed.children[i].style.backgroundImage = `url(${quizzes[i].image})`;
     }
+    SyncLayout();
 }
 
-function goToQuizz(quizz){
+function goToQuizz(quizz) {
     id = quizz.getAttribute("id");
     console.log(id);
-    window.location.href = 'quizz.html';
+    window.location.href = 'quizz.html?id=' + id;
 }
 
-function render() {
+function SyncLayout() {
+    /*se não tiver quizz add hidden em seus quizzes*/
+    // if (localQuizzes === null) {
+    //     withQuizz.classList.add("hidden");
+    //     alert();
+    // } /* senão add none em add hidden em criar quizz*/
+    // else {
+    //     alert();
+    //     NoQuizz.classList.add("hidden");
+    //     renderyourQuizzes();
+    // }
+}
+function renderyourQuizzes() {
+
+}
+
+async function deleteQuizz(quizz) {
+    alert();
+    try {
+        let options = {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Secret-Key": quizz.key ? quizz.key : ""
+            }
+        };
+        let request = await fetch("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/" + quizz.id, options);
+        if (request.ok) {
+            let userQuizzes = JSON.parse(localStorage.getItem("quizzes"));
+            let userQuizz = userQuizzes.filter(userQuizz => userQuizz.id === quizz.id);
+            let indexOf = Array.prototype.indexOf.call(userQuizzes, userQuizz[0]);
+            userQuizzes.splice(indexOf, 1);
+            localStorage.setItem(myQuizzes, JSON.stringify(userQuizzes));
+        }
+        await renderizeQuizzes();
+    } catch (error) {
+        window.location.reload();
+    }
 
 }
